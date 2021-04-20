91282a3de8c6e4f3f28b361c18fa07f61014646b
bagussamito <bagussamito1@gmail.com>
2021-04-20 22:08:03 +0700

    a

--- app/src/main/java/com/example/studikasus/RegisterActivity.kt
+++ app/src/main/java/com/example/studikasus/RegisterActivity.kt
@@ -1,7 +1,10 @@
 package com.example.studikasus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_register.*
 import com.google.firebase.auth.FirebaseAuth
 
@@ -17,8 +20,41 @@ class RegisterActivity : AppCompatActivity() {
     auth = FirebaseAuth.getInstance()
 
         btnRegister.setOnClickListener{
-            val email = etEmail
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (email.isEmpty()){
+                etEmail.error = "Email harus Diisi"
+                etEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                etEmail.error = "Email harus Diisi"
+                etEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.isEmpty() || password.length < 6){
+                etPassword.error = "Password harus lebih dari 6 karakter"
+                etPassword.requestFocus()
+                return@setOnClickListener
+            }
+            registerUser(email, password)
+        }
         }
 
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Intent(this@RegisterActivity, HomeActivity::class.java).also {
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
+                    }
+                } else {
+                    Toast.makeText(this, it.exception.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
+
 }
