b12be0685d444d2e23d12711921e766dc847fb8b
bagussamito <bagussamito1@gmail.com>
2021-04-23 14:33:43 +0700

    Update toolbar logout

--- app/src/main/java/com/example/studikasus/HomeActivity.kt
+++ app/src/main/java/com/example/studikasus/HomeActivity.kt
@@ -1,16 +1,20 @@
 package com.example.studikasus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlin.system.exitProcess
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var bottomNav: ChipNavigationBar
     private lateinit var setelanNav: SetelanFragment
+    private lateinit var tblogout : Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,15 @@ class HomeActivity : AppCompatActivity() {
 
         bottomMenu()
 
+        tblogout = findViewById(R.id.toolbarLogout)
+
+        tblogout.setOnClickListener(){
+            val intent = Intent (this@HomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 
     private fun bottomMenu() {
         bottomNav.setOnItemSelectedListener(object :
@@ -45,6 +57,5 @@ class HomeActivity : AppCompatActivity() {
             }
         })
     }
-
 
 }
