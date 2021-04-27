8cac305ac9a26d73e10623f4c1fa258203164f0a
bagussamito <bagussamito1@gmail.com>
2021-04-27 23:04:58 +0700

    RV wes dadi kabeh

--- app/src/main/java/com/example/studikasus/DaftarPengeluaranActivity.kt
+++ app/src/main/java/com/example/studikasus/DaftarPengeluaranActivity.kt
@@ -2,10 +2,53 @@ package com.example.studikasus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studikasus.fragments.adapters.AdapterPemasukan
+import com.example.studikasus.fragments.adapters.AdapterPengeluaran
+import com.google.firebase.database.*
 
 class DaftarPengeluaranActivity : AppCompatActivity() {
+
+    private lateinit var dbref : DatabaseReference
+    private lateinit var pengeluaranRecyclerview : RecyclerView
+    private lateinit var pengeluaranArraylist : ArrayList<Item_Pengeluaran>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daftar_pengeluaran)
+
+        pengeluaranRecyclerview = findViewById(R.id.pengeluaran_list)
+        pengeluaranRecyclerview.layoutManager = LinearLayoutManager(this)
+        pengeluaranRecyclerview.setHasFixedSize(true)
+
+        pengeluaranArraylist = arrayListOf()
+        getUserData()
+    }
+
+    private fun getUserData() {
+
+        dbref = FirebaseDatabase.getInstance().getReference("Pengeluaran")
+
+        dbref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+
+                    for (pengeluaranSnapshot in snapshot.children){
+
+                        val keluaran = pengeluaranSnapshot.getValue(Item_Pengeluaran::class.java)
+                        pengeluaranArraylist.add(keluaran!!)
+                    }
+                    pengeluaranRecyclerview.adapter =
+                            AdapterPengeluaran(
+                                    pengeluaranArraylist
+                            )
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/studikasus/fragments/adapters/AdapterPengeluaran.kt
@@ -0,0 +1,42 @@
+package com.example.studikasus.fragments.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studikasus.Item_Pengeluaran
+import com.example.studikasus.R
+import com.example.studikasus.fragments.adapters.AdapterPengeluaran.*
+
+class AdapterPengeluaran(private val listpengeluaran : ArrayList<Item_Pengeluaran>): RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val pengeluaranView = LayoutInflater.from(parent.context).inflate(
+                R.layout.pengeluaran_item,
+                parent, false)
+        return MyViewHolder(
+                pengeluaranView
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return listpengeluaran.size
+    }
+
+
+    class  MyViewHolder(pengeluaranView : View) : RecyclerView.ViewHolder(pengeluaranView){
+
+        val kategori : TextView = pengeluaranView.findViewById(R.id.kategoritext)
+        val nominal : TextView = pengeluaranView.findViewById(R.id.totalnumber)
+        val deskripsi : TextView = pengeluaranView.findViewById(R.id.deskripsitext)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentitem = listpengeluaran[position]
+
+        holder.kategori.text = currentitem.kategori
+        holder.nominal.text = currentitem.nominal
+        holder.deskripsi.text = currentitem.deskripsi
+    }
+}
